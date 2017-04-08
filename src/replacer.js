651ef768409c8b21222c9d303fe1b5bc04d99b53
Eduardo Born <nosachamos@gmail.com>
2017-04-08 17:00:01 -0500

    Many fixes for advanced cases, support for replacing inside media queries and building to UMD format

--- src/replacer.js
+++ src/replacer.js
@@ -10,9 +10,9 @@ export default class Replacer {
      * @param {Object} config
      * @param {String} config.prefix Used when CSS classes look like "{prefix}profile" "d-profile"
      * @param {RegExp} config.regexp Used when CSS classes use non-prefix declaration format.
-     * @param {Function} config.replace 
-     * Function that will be called for each node from JS-AST. It's used when regexp is not enough and you need to 
-     * replace CSS classes based on some specific rules. 
+     * @param {Function} config.replace
+     * Function that will be called for each node from JS-AST. It's used when regexp is not enough and you need to
+     * replace CSS classes based on some specific rules.
      * For example:
      * @example
      * In Sencha Touch / ExtJS, when you define a component:
@@ -22,13 +22,13 @@ export default class Replacer {
      *          baseCls: "d-my-component"
      *      }
      *  });
-     * ST/ExtJS will construct an additional CSS class "d-my-component-inner" using a rule #baseCls + '-inner', so 
+     * ST/ExtJS will construct an additional CSS class "d-my-component-inner" using a rule #baseCls + '-inner', so
      * when you replace #baseCls you need to replace "d-my-component-inner" with a minimized version of #baseCls plus
-     * '-inner' string to avoid bugs. So it could look like this: 
+     * '-inner' string to avoid bugs. So it could look like this:
      * "d-my-component" -> "a0"
      * "d-my-component-inner" -> "a0-inner"
      *
-     * @param {Boolean} replaceAll Should be true to rename all CSS classes including those classes that couldn't be 
+     * @param {Boolean} replaceAll Should be true to rename all CSS classes including those classes that couldn't be
      *                             found in js-file (probably unused).
      */
     constructor(config) {
@@ -39,19 +39,19 @@ export default class Replacer {
             replace: this.emptyFn,
             replaceAll: false
         }, config);
-        
-        this.key = "a0";
+
+        this.key = "_";
         this.replacements = {
             count: 0,
             items: {}
         };
     }
-    
+
     /**
      * a reusable empty function
      */
     emptyFn() {}
-    
+
     /**
      * @param {String}
      * @return {String}
@@ -60,14 +60,32 @@ export default class Replacer {
         let className = 'no-match';
         while (true) {
             className = this.counter.toString(34);
-            if (this.cssText.search(new RegExp('\\b\\.' + className + '\\b', 'gi')) > -1) {
+            // only accept valid class names (cannot start with a digit, or a hyphen followed by a digit)
+            if (!/^([a-z_]|-[a-z_-])[a-z\d_-]*$/i.test(className)) {
+              this.counter++;
+              continue;
+            }
+
+            console.log('COUNTER: '+ this.counter);
+            console.log('CLASS NAME: '+ className);
+
+            let regex = '\\b\\.' + className + '\\b';
+            console.log('REGEX: '+ regex);
+
+            if (this.cssText.search(new RegExp(regex, 'gi')) > -1) {
+                console.log('FOUND A CLASH');
                 this.counter++;
-            } else break;
+
+            } else {
+                console.log('NO CLASH');
+                break;
+            }
         }
+        this.counter++;
 
         return className;
     }
-    
+
     /**
      * an entry point.
      */
@@ -77,7 +95,7 @@ export default class Replacer {
         this.parseCssRules();
         this.replace();
     }
-    
+
     /**
      * simply reads the content of CSS and js file.
      */
@@ -85,7 +103,7 @@ export default class Replacer {
         this.cssText = fs.readFileSync(this.config.cssIn, "utf8");
         this.jsText = fs.readFileSync(this.config.jsIn, "utf8");
     }
-    
+
     /**
      * initializes AST for both CSS and JS.
      */
@@ -93,7 +111,7 @@ export default class Replacer {
         this.cssAst = css.parse(this.cssText);
         this.jsAst = esprima.parse(this.jsText);
     }
-    
+
     /**
      * @return {RegExp} regexp to match CSS classes like: .d-user-profile
      */
@@ -108,7 +126,7 @@ export default class Replacer {
 
         return new RegExp("\\.[0-9a-zA-Z\\-_]+", "g");
     }
-    
+
     /**
      * @return {RegExp} regexp to match CSS classes like: <div class="d-user-profile"></div>
      */
@@ -117,13 +135,13 @@ export default class Replacer {
 
         if(config.prefix)
             return new RegExp("(\\b" + config.prefix + "[0-9a-zA-Z\-_]+)", "g");
-        
+
         if(config.regexp)
             return config.regexp;
 
         return new RegExp(this.classes.join("|"), "g");
     }
-    
+
     /**
      * parses CSS file to extract all CSS class names in required order.
      */
@@ -135,13 +153,21 @@ export default class Replacer {
         this.rules = this.cssAst.stylesheet.rules;
 
         for(var i=0, rule; rule=this.rules[i]; i++) {
-            if(rule.type != "rule")
-                continue;
+            if (rule.type == "media") {
+                // for (var j = 0, mediaRule; mediaRule = rule.rules[j]; j++) {
+                //     if (mediaRule.type != "rule") continue;
+                //     var selectors = mediaRule.selectors.join(" ").match(regexp);
+                //     if (selectors) classes = classes.concat(selectors.join(" ").replace(/\./g, "").split(" "));
+                // }
 
-            var selectors = rule.selectors.join(" ").match(regexp);
+            } else {
+                if(rule.type != "rule") continue;
 
-            if(selectors)
-                classes = classes.concat(selectors.join(" ").replace(/\./g, "").split(" "));
+                var selectors = rule.selectors.join(" ").match(regexp);
+
+                if(selectors)
+                    classes = classes.concat(selectors.join(" ").replace(/\./g, "").split(" "));
+            }
         }
 
         this.classes = classes.sort(function(a, b) {
@@ -150,7 +176,7 @@ export default class Replacer {
             return classes.indexOf(cls) == pos;
         });
     }
-    
+
     /**
      * replaces CSS class names in JS AST
      * @return {Replacer}
@@ -179,7 +205,7 @@ export default class Replacer {
 
         return this;
     }
-    
+
     /**
      * Replaces a CSS class name in string literal node with it's minimized version.
      * @param {Object} node String literal node.
@@ -198,7 +224,7 @@ export default class Replacer {
         for(var i=0, match; match=matches[i]; i++) {
             if(! replacements.items[match]) {
                 replacements.items[match] = key;
-                key = this.succ(key);
+                key = this.succ();
             }
         }
 
@@ -210,7 +236,7 @@ export default class Replacer {
         node.value = value;
         this.key = key;
     }
-    
+
     /**
      * peforms replacements for all unmatched CSS class names.
      * @return {undefined}
@@ -227,7 +253,7 @@ export default class Replacer {
             }
         });
     }
-    
+
     /**
      * @returns {String} Resulting CSS code with replacements based on CSS AST.
      */
@@ -236,34 +262,74 @@ export default class Replacer {
             regexp = this.generateCssClsRegExp();
 
         for(var i=0, rule; rule=this.rules[i]; i++) {
-            if(rule.type != "rule")
-                continue;
+            if (rule.type == "media") {
+                for(var z=0, mediaRule; mediaRule=rule.rules[z]; z++) {
+                    if (mediaRule.type != "rule") continue;
 
-            var newSelectors = [];
+                    var newSelectors = [];
 
-            for(var j=0, selector; selector=rule.selectors[j]; j++) {
-                selector = selector.replace(regexp, function(a) {
-                    return "." + replacements.items[a.replace(".", "")];
-                });
-                
-                if(/undefined/.test(selector))
-                    // it can mean two things:
-                    // 1. there is a CSS rule which is not used in js file.
-                    // 2. it's a bug in gulp-css-gsub :)
-                    console.log("undefined in " + selector + " === " + rule.selectors.join(" "))
-                else
-                    newSelectors.push(selector);
+                    for (var j=0, selector; selector=mediaRule.selectors[j]; j++) {
+                        selector = selector.replace(regexp, function(a) {
+                            var clazz = a.replace(".", "");
+                            if (!replacements.items[clazz] && mediaRule.selectors.join(" ").indexOf(':not') > -1) {
+                                replacements.items[clazz] = self.succ();
+                            }
+                            return "." + replacements.items[clazz];
+                        });
+
+                        if(/undefined/.test(selector)) {
+                            // it can mean two things:
+                            // 1. there is a CSS rule which is not used in js file.
+                            // 2. it's a bug in gulp-css-gsub :)
+                            console.log("undefined in " + selector + " === " + mediaRule.selectors.join(" "))
+                        } else {
+                            newSelectors.push(selector);
+                        }
+                    }
+
+                    if (newSelectors.length == mediaRule.selectors.length) {
+                        mediaRule.selectors = newSelectors;
+                    } else {
+                        mediaRule.selectors = []; // remove mediaRule, because of unused selector.
+                    }
+                }
+
+            } else {
+                if (rule.type != "rule") continue;
+
+                var newSelectors = [];
+
+                for (var j=0, selector; selector=rule.selectors[j]; j++) {
+                    selector = selector.replace(regexp, function(a) {
+                        var clazz = a.replace(".", "");
+                        if (!replacements.items[clazz] && rule.selectors.join(" ").indexOf(':not') > -1) {
+                            replacements.items[clazz] = self.succ();
+                        }
+                        return "." + replacements.items[clazz];
+                    });
+
+                    if(/undefined/.test(selector)) {
+                        // it can mean two things:
+                        // 1. there is a CSS rule which is not used in js file.
+                        // 2. it's a bug in gulp-css-gsub :)
+                        console.log("undefined in " + selector + " === " + rule.selectors.join(" "))
+                    } else {
+                        newSelectors.push(selector);
+                    }
+                }
+
+                if (newSelectors.length == rule.selectors.length) {
+                    rule.selectors = newSelectors;
+                } else {
+                    rule.selectors = []; // remove rule, because of unused selector.
+                }
             }
-
-            if(newSelectors.length == rule.selectors.length) 
-                rule.selectors = newSelectors;
-            else
-                rule.selectors = []; // remove rule, because of unused selector.
+            
         }
 
         return css.stringify(this.cssAst);
     }
-    
+
     /**
      * @returns {String} a resulting JS code with replacements based on JS AST.
      */
